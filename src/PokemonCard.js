a268c62a4429620ea324c4125b8919a02aea8f37
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-06-15 15:02:08 -0300

    Add type and image field to Card
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Card from 'react-bootstrap/Card'
 
 class PokemonCard extends React.Component {
+  validateTypes(){
+    if (this.props.pokemon.types != undefined)
+      return (this.props.pokemon.types).join();
+    else
+      return "";
+  }
+
 	render() {
 		return (
 			<Card style={{ width: '18rem', marginBottom: '10px' }}>
-        <Card.Img variant="top" src={this.props.pokemon.image} />
+        <Card.Img variant="top" src="https://picsum.photos/200" />
         <Card.Body>
           <Card.Title>{this.props.pokemon.name}</Card.Title>
           <Card.Text>
-          {this.props.pokemon.type}
+            {this.validateTypes()}
           </Card.Text>
         </Card.Body>
 			</Card>
